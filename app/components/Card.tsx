3cb815c2bc5a572141a2e64c9c557aaa0ab845f0
TintinLeGrand <ethan.leneindre9@gmail.com>
2025-01-11 17:17:45 +0100

    feat: Add themes with Flowbite and react themes

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,9 +1,29 @@
 import { Card } from "flowbite-react";
 import Image from "next/image";
-import Link from 'next/link'
 import { Suspense } from "react";
 import membersData from "@/lib/members.json";
 import { ReversedButton } from "./Buttons";
+import type { CustomFlowbiteTheme } from "flowbite-react";
+
+const customTheme: CustomFlowbiteTheme['card'] = {
+    root: {
+        "base": "rounded-xl max-w-lg h-full cursor-pointer gap-2 border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent",
+        "children": "flex h-fill flex-col justify-center gap-4 p-6",
+        "horizontal": {
+            "off": "flex-col",
+            "on": "flex-col md:max-w-xl md:flex-row"
+        },
+        "href": "hover:bg-gray-100 dark:hover:bg-gray-700"
+    },
+    "img": {
+        "base": "",
+        "horizontal": {
+            "off": "rounded-t-lg",
+            "on": "h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+        }
+    }
+
+};
 
 export function MemberCard(props: { name: string }) {
     const member = membersData.members.find(
@@ -14,10 +34,10 @@ export function MemberCard(props: { name: string }) {
         return (
             <Card className="max-w-sm">
                 <div className="flex flex-col items-center pb-10">
-                    <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                    <h5 className="mb-1 text-xl font-medium">
                         Membre introuvable
                     </h5>
-                    <span className="text-sm text-gray-500 dark:text-gray-400">
+                    <span className="text-sm">
                         Le prénom "{props.name}" ne correspond à aucun membre.
                     </span>
                 </div>
@@ -26,7 +46,7 @@ export function MemberCard(props: { name: string }) {
     }
 
     return (
-        <Card className="max-w-sm h-full">
+        <Card theme={customTheme}>
             <div className="flex flex-col items-center py-8">
                 <Suspense fallback={<p>Loading...</p>}>
                     <Image
@@ -38,15 +58,15 @@ export function MemberCard(props: { name: string }) {
                     />
                 </Suspense>
 
-                <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                <h5 className="mb-1 text-xl font-medium underline-offset-4 underline">
                     {member.name.pseudo}
                 </h5>
-                <span className="text-sm text-gray-500 dark:text-gray-400">
+                <span className="text-sm text-justify">
                     {member.description || "4Fromages developper."}
                 </span>
                 <div className="mt-4 flex space-x-3 lg:mt-6">
-                    <ReversedButton href={member.github} text="GitHub" reversed/>
-                    <ReversedButton href={`mailto:${member.email}`} text="Email" target="_blank"/>
+                    <ReversedButton href={member.github} text="GitHub" reversed />
+                    <ReversedButton href={`mailto:${member.email}`} text="Email" target="_blank" />
                 </div>
             </div>
         </Card>
--- app/components/PopoverProject.tsx
+++ app/components/PopoverProject.tsx
@@ -2,18 +2,26 @@
 
 import { Dropdown } from "flowbite-react";
 import { IoIosLink } from "react-icons/io";
-import { RiBlueskyLine, RiGithubLine  } from "react-icons/ri";
+import { RiBlueskyLine, RiGithubLine } from "react-icons/ri";
+import type { CustomFlowbiteTheme } from "flowbite-react";
+
+const customTheme: CustomFlowbiteTheme['popover'] = {
+  base: "bg-none hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a]",
+  content: "cursor-pointer gap-2 border border-solid border-black/[.08] dark:border-white/[.145] transition-colors bg-[#ffffff] dark:bg-[#0a0a0a] text-sm sm:text-base",
+
+
+};
 
 export function DropdownLink() {
   return (
-    <Dropdown className="p-0 m-0 border-none bg-none" dismissOnClick={true} renderTrigger={() =>
+    <Dropdown theme={customTheme} dismissOnClick={true} renderTrigger={() =>
       <div className="rounded-full cursor-pointer gap-2 border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44">
         <IoIosLink/>
         <p>Our links</p>
       </div>
     }>
-      <Dropdown.Item className="cursor-pointer border-black/[.08] dark:border-white/[.145] flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a]" icon={RiBlueskyLine} as="a" href="https://bsky.app/profile/4fromages.fr" target="_blank">Bluesky</Dropdown.Item>
-      <Dropdown.Item className="cursor-pointer border-black/[.08] dark:border-white/[.145] flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a]" icon={RiGithubLine } as="a" href="https://github.com/4Fromages" target="_blank">GitHub</Dropdown.Item>
+      <Dropdown.Item icon={RiBlueskyLine} as="a" href="https://bsky.app/profile/4fromages.fr" target="_blank">Bluesky</Dropdown.Item>
+      <Dropdown.Item icon={RiGithubLine} as="a" href="https://github.com/4Fromages" target="_blank">GitHub</Dropdown.Item>
       {/* <Dropdown.Item>Earnings</Dropdown.Item>
       <Dropdown.Item>Sign out</Dropdown.Item> */}
     </Dropdown>
